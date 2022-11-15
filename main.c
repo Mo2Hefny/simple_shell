21190ca016fd64252c98c0d9fff62b00065dc77f
Mo2Hefny <moamensaied1@gmail.com>
2022-11-15 06:38:05 -0800

    separated loop

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
 #include "main.h"
 
 /**
+ * shell_interact - loop for user end.
+ * @buffer: buffer string.
+ * @paths: array of PATH strings.
+ * @buffsize: buffer length.
+ */
+
+void shell_interact(char *buffer, char **paths, size_t buffsize)
+{
+char **tokens;
+int piped = 0;
+
+do {
+if (isatty(0))
+{
+_puts("#cisfun$ ");
+piped = 1;
+}
+
+if (getline(&buffer, &buffsize, stdin) == -1)
+{
+_putchar('\n');
+break;
+}
+
+buffer[strlen(buffer) - 1] = '\0';
+tokens = buffer_translator(buffer);
+
+/* checks build-ins and frees tokens before recursion */
+if (check_builtin(tokens))
+continue;
+
+check_path(paths, tokens);
+free(tokens);
+} while (piped);
+free(buffer);
+free(paths);
+}
+
+/**
  * main - Simple shell.
  * @argc: number of arguments.
  * @argv: argument strings.
@@ -9,13 +48,16 @@
 
 int main(int argc, char **argv)
 {
-char *buffer, *path;
-char **tokens, **paths;
-size_t piped = 0, buffersize = 1024;
+char *buffer, *path, **paths;
+size_t buffersize = 1024;
+info_t info;
+info.program = argv[0];
+info.linecount = 0;
+
 if (argc > 1)
 if (execve(argv[1], argv, environ) == -1)
 {
-perror(argv[0]);
+perror(info.program);
 exit(-1);
 }
 buffer = malloc(buffersize);
@@ -26,25 +68,7 @@ if (!buffer)
 perror("Unable to allocate buffer");
 exit(1);
 }
-do {
-if (isatty(0))
-{
-piped = 1;
-printf("#cisfun$ ");
-}
-if (getline(&buffer, &buffersize, stdin) == -1)
-{
-putchar('\n');
-break;
-}
-buffer[strlen(buffer) - 1] = '\0';
-tokens = buffer_translator(buffer);
-if (check_builtin(tokens))
-continue;
-check_path(paths, tokens);
-free(tokens);
-} while (piped);
-free(buffer);
-free(paths);
+shell_interact(buffer, paths, buffersize);
+
 return (0);
 }
