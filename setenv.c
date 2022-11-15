853e2ab46f9811148bbdec8a709ba341463e42b2
Mo2Hefny <moamensaied1@gmail.com>
2022-11-15 06:34:59 -0800

    hot fix

--- setenv.c
+++ setenv.c
@@ -10,11 +10,11 @@ int set_env(char **cmd)
 {
 if (!cmd[1] || !cmd[2])
 {
-fprintf(stderr, "Not enough arguments");
+fprintf(stderr, "Not enough arguments\n");
 return (1);
 }
 if (setenv(cmd[1], cmd[2], 1) == -1)
-fprintf(stderr, "No variable declared");
+fprintf(stderr, "No variable declared\n");
 return (1);
 }
 
@@ -27,8 +27,8 @@ return (1);
 int unset_env(char **cmd)
 {
 if (!cmd[1])
-fprintf(stderr, "No variable declared");
+fprintf(stderr, "No variable declared\n");
 else if (unsetenv(cmd[1]) == -1)
-fprintf(stderr, "Variable not found");
+fprintf(stderr, "Variable not found\n");
 return (1);
 }
