2a9b95d9e68aac9bce18e04668d512759c11c2e2
Mo2Hefny <moamensaied1@gmail.com>
2022-11-15 08:27:07 -0800

    added cd

--- builtin_func.c
+++ builtin_func.c
@@ -31,3 +31,50 @@ exit(0);
 n = atoi(cmd[1]);
 exit(n);
 }
+
+/**
+ * change_dir - cd build-in
+ * @cmd: tokens
+ * Return: 1
+ */
+
+int change_dir(char **cmd)
+{
+char *prev, *current;
+
+prev = get_env_variable("PWD");
+
+if (!cmd[1])
+{
+if (chdir("/root") == -1)
+{
+perror("Error ~");
+return (1);
+}
+current = "/root";
+}
+else if (cmd[1][0] == '-')
+{
+current = get_env_variable("OLDPWD");
+if (chdir(current) == -1)
+{
+perror("Error -");
+return (1);
+}
+}
+else
+{
+if (chdir(cmd[1]) == -1)
+{
+perror("Error cd");
+return (1);
+}
+current = cmd[1];
+}
+if (current)
+{
+setenv("PWD", current, 1);
+setenv("OLDPWD", prev, 1);
+}
+return (1);
+}
