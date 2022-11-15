42ce232bee54aff2503230518d74889c7150ab4c
Mo2Hefny <moamensaied1@gmail.com>
2022-11-15 06:30:31 -0800

    cleaned functions

--- check_builtin.c
+++ check_builtin.c
@@ -9,34 +9,22 @@
 int handle_builtin(char **cmd)
 {
 if (strncmp(cmd[0], "env", 3) == 0)
-{
-print_env();
-return (1);
-}
+return (print_env());
+
 else if (strncmp(cmd[0], "exit", 4) == 0)
-{
-if (cmd[1] == NULL)
-exit(0);
-exit_func(cmd[1]);
-return (1);
-}
+exit_func(cmd);
+
 else if (strncmp(cmd[0], "setenv", 6) == 0)
-{
-if (!cmd[1] || !cmd[2])
-{
-fprintf(stderr, "Not enough arguments");
-return (1);
-}
-else
-return (set_env(cmd[1], cmd[2], 1));
-}
+return (set_env(cmd));
+
 else if (strncmp(cmd[0], "unsetenv", 8) == 0)
 return (unset_env(cmd));
+
 return (0);
 }
 
 /**
- * check_builtin _ checks for built in commands.
+ * check_builtin - checks for built in commands.
  * @cmd: commands array.
  * Return: 1 if found built in command, 0 other wise.
  */
